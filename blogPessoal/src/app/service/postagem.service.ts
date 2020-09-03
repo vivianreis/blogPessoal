05fb563ad90d0e7fa263aae0c4883c00607e889b
vivianreis <srtvivian@hotmail.com>
2020-09-03 14:03:33 -0300

    delete tema e postagem

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -14,19 +14,23 @@ export class PostagemService {
   }
 
   getAllPostagens(){
-    return this.http.get('http://localhost:8080/postagens', this.token)
+    return this.http.get('http://localhost:9000/postagens', this.token)
   }
 
   getByIdPostagem(id: number){
-    return this.http.get(`http://localhost:9000/tema/${id}`, this.token)
+    return this.http.get(`http://localhost:9000/postagens/${id}`, this.token)
   }
 
   postPostagem(postagem: Postagem){
-    return this.http.post('http://localhost:8080/postagens', postagem, this.token)
+    return this.http.post('http://localhost:9000/postagens', postagem, this.token)
   }
 
   putPostagem(postagem: Postagem){
-    return this.http.put('http://localhost:8080/postagens', postagem, this.token)
+    return this.http.put('http://localhost:9000/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`http://localhost:9000/postagens/${id}`, this.token)
   }
     
 }
